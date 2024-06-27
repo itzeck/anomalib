dacbcbbdd9250eb77c097f9d9e5fbe1a39cd4d58
itzeck <105644082+itzeck@users.noreply.github.com>
2024-06-27 14:51:20 +0200

    Update lightning_model.py

--- src/anomalib/models/image/efficient_ad/lightning_model.py
+++ src/anomalib/models/image/efficient_ad/lightning_model.py
@@ -132,30 +132,29 @@ class EfficientAd(AnomalyModule):
         n: torch.Tensor | None = None
         chanel_sum: torch.Tensor | None = None
         chanel_sum_sqr: torch.Tensor | None = None
-        self.model.eval()
-        with torch.no_grad():
-            for batch in tqdm.tqdm(dataloader, desc="Calculate teacher channel mean & std", position=0, leave=True):
-                y = self.model.teacher(batch["image"].to(self.device))
-                if not arrays_defined:
-                    _, num_channels, _, _ = y.shape
-                    n = torch.zeros((num_channels,), dtype=torch.int64, device=y.device)
-                    chanel_sum = torch.zeros((num_channels,), dtype=torch.float32, device=y.device)
-                    chanel_sum_sqr = torch.zeros((num_channels,), dtype=torch.float32, device=y.device)
-                    arrays_defined = True
-    
-                n += y[:, 0].numel()
-                chanel_sum += torch.sum(y, dim=[0, 2, 3])
-                chanel_sum_sqr += torch.sum(y**2, dim=[0, 2, 3])
-    
-            if n is None:
-                msg = "The value of 'n' cannot be None."
-                raise ValueError(msg)
-    
-            channel_mean = chanel_sum / n
-    
-            channel_std = (torch.sqrt((chanel_sum_sqr / n) - (channel_mean**2))).float()[None, :, None, None]
-            channel_mean = channel_mean.float()[None, :, None, None]
-        self.model.train()
+
+        for batch in tqdm.tqdm(dataloader, desc="Calculate teacher channel mean & std", position=0, leave=True):
+            y = self.model.teacher(batch["image"].to(self.device))
+            if not arrays_defined:
+                _, num_channels, _, _ = y.shape
+                n = torch.zeros((num_channels,), dtype=torch.int64, device=y.device)
+                chanel_sum = torch.zeros((num_channels,), dtype=torch.float32, device=y.device)
+                chanel_sum_sqr = torch.zeros((num_channels,), dtype=torch.float32, device=y.device)
+                arrays_defined = True
+
+            n += y[:, 0].numel()
+            chanel_sum += torch.sum(y, dim=[0, 2, 3])
+            chanel_sum_sqr += torch.sum(y**2, dim=[0, 2, 3])
+
+        if n is None:
+            msg = "The value of 'n' cannot be None."
+            raise ValueError(msg)
+
+        channel_mean = chanel_sum / n
+
+        channel_std = (torch.sqrt((chanel_sum_sqr / n) - (channel_mean**2))).float()[None, :, None, None]
+        channel_mean = channel_mean.float()[None, :, None, None]
+
         return {"mean": channel_mean, "std": channel_std}
 
     @torch.no_grad()
@@ -172,20 +171,19 @@ class EfficientAd(AnomalyModule):
         maps_st = []
         maps_ae = []
         logger.info("Calculate Validation Dataset Quantiles")
-        self.model.eval()
-        with torch.no_grad():
-            for batch in tqdm.tqdm(dataloader, desc="Calculate Validation Dataset Quantiles", position=0, leave=True):
-                for img, label in zip(batch["image"], batch["label"], strict=True):
-                    if label == 0:  # only use good images of validation set!
-                        output = self.model(img.to(self.device), normalize=False)
-                        map_st = output["map_st"]
-                        map_ae = output["map_ae"]
-                        maps_st.append(map_st)
-                        maps_ae.append(map_ae)
-    
-            qa_st, qb_st = self._get_quantiles_of_maps(maps_st)
-            qa_ae, qb_ae = self._get_quantiles_of_maps(maps_ae)
-        self.model.train()
+
+        for batch in tqdm.tqdm(dataloader, desc="Calculate Validation Dataset Quantiles", position=0, leave=True):
+            for img, label in zip(batch["image"], batch["label"], strict=True):
+                if label == 0:  # only use good images of validation set!
+                    output = self.model(img.to(self.device), normalize=False)
+                    map_st = output["map_st"]
+                    map_ae = output["map_ae"]
+                    maps_st.append(map_st)
+                    maps_ae.append(map_ae)
+
+        qa_st, qb_st = self._get_quantiles_of_maps(maps_st)
+        qa_ae, qb_ae = self._get_quantiles_of_maps(maps_ae)
+
         return {"qa_st": qa_st, "qa_ae": qa_ae, "qb_st": qb_st, "qb_ae": qb_ae}
 
     def _get_quantiles_of_maps(self, maps: list[torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor]:
